Corrected Interpolation Formula

First_step/bigram_model_interpolation.py
@@ -1,16 +1,16 @@
 import nltk
-import csv
 from nltk import bigrams
 from nltk.book import text1
 import numpy as np
 import pandas as pd
+import csv
+
+#Bigram Count and Unigram Count
 model= {}
 text = [x.lower() for x in text1]
-#text = ['<s>']+text+['<\s>']
-bi = list(bigrams(text, pad_right=False, pad_left=False))
+bi = list(bigrams(text))
 uni = nltk.FreqDist(text)
 length = len(text)
-#print(uni)
 for w1,w2 in bi:
     if w1 in model:
         if w2 in model[w1]:
@@ -20,22 +20,28 @@ for w1,w2 in bi:
     else:
         model[w1] = {}
         model[w1][w2]=1
-print(len(uni))
+
+#Encode and Decode words to index for numpy array
 encode = {i:w[0] for i,w in enumerate(uni.most_common())}
 decode = {w[0]:i for i,w in enumerate(uni.most_common())}
-#print(encode[1],decode[encode[1]])
+
+#Lambda for smoothing using Interpolation(Jelinek-Mercer)
 lamda = 0.9
+
+#Smoothing Applied
 mod = np.zeros(shape=(len(uni),len(uni)))
 for i in range(len(uni)):
     for j in range(len(uni)):
         if encode[i] not in model or encode[j] not in model[encode[i]]:
-            mod[i][j]= (1-lamda)*uni[encode[i]]/length
+            mod[i][j]= (1-lamda)*uni[encode[j]]/length
         else:
-            mod[i][j]=lamda*(model[encode[i]][encode[j]])/(uni[encode[i]])+(1-lamda)*uni[encode[i]]/length
+            mod[i][j]=lamda*(model[encode[i]][encode[j]])/(uni[encode[i]])+(1-lamda)*uni[encode[j]]/length
 
+#Convert the numpy matrix to pandas dataframe
 data = [w[0] for w in uni.most_common()]
 df = pd.DataFrame(data=mod[:25,:25], index=data[:25], columns=data[:25])
-print(df[encode[1]])
+
+#Write to File
 with open('probs.csv', 'w') as csvfile:
     df.to_csv(csvfile)
 print("Done")